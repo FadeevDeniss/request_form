29933a1d8ba3194acc6f4396dcc41a6157118faf
Fadeev <deniro094@gmail.com>
2021-11-14 16:05:25 +0300

    message/models.py: fix parameters

--- ticket/message/models.py
+++ ticket/message/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Credentials(models.Model):
-    name = models.CharField(label='Your name', max_length=50)
-    description = models.TextField(label='Problem description', max_length=250)
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=250)
     email = models.EmailField()
-    checkbox = models.BooleanField(name='Personal data processing agreement')
+    checkbox = models.BooleanField()
     created_at = models.DateTimeField(auto_now_add=True)
 
