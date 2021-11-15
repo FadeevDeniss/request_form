335cfb1f505a96f74e77d937c892bb8710e00518
Fadeev <deniro094@gmail.com>
2021-11-15 23:02:14 +0300

    Set options for email sending

--- ticket/ticket/settings.py
+++ ticket/ticket/settings.py
@@ -124,3 +124,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'deniro094@gmail.com'
+EMAIL_HOST_PASSWORD = 'kisa16012010'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
